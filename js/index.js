d572ec5a8f6acced2d6d7b5489c556994a32e51e
lily4178993 <nellytelli@gmail.com>
2023-06-12 13:31:49 +0000

    Imported books-form module functions to index.js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Importing formValidation function from '../modules/books-form.js'
+import formValidation from '../modules/books-form.js';
+
+// Get the form submission
+const form = document.querySelector('form');
+
+// Event listener for the form submission
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Call the form validation function
+  formValidation();
+});
